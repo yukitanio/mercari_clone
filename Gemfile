a3bb3973256d3e955b43862a258d21bc4e3a1a5e
Yuki Tanio <yuki_tanio@yahoo.co.jp>
2019-07-27 08:53:29 +0900

    ユーザーモデル作成、新規登録、ログイン機能実装

--- Gemfile
+++ Gemfile
@@ -17,7 +17,9 @@ gem 'jquery-rails'
 gem 'carrierwave'
 gem 'ransack'
 gem 'rails-i18n'
-
+gem 'devise'
+gem 'devise-i18n'
+gem 'devise-i18n-views'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root "products#top"
+  # devise_for :users
   resources :products
   get "/category_list", to: 'products#category_list'
   post "/product/:id/transaction_status", to: 'products#transaction_status'
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords'
+  } 
 end
