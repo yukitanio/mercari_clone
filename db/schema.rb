058e1bc0923200ea37e972d4fa06fd4d734098cf
Yuki Tanio <yuki_tanio@yahoo.co.jp>
2019-07-29 17:45:36 +0900

    Profileモデルの作成とUserモデルとの関連付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_29_020600) do
+ActiveRecord::Schema.define(version: 2019_07_29_082049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,16 @@ ActiveRecord::Schema.define(version: 2019_07_29_020600) do
     t.integer "transaction_status", default: 0
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.string "name"
+    t.string "picture"
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -51,4 +61,5 @@ ActiveRecord::Schema.define(version: 2019_07_29_020600) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
