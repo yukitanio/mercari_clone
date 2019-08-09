03fa0563e696d81bff529cfae26de68ac245c97c
Yuki Tanio <yuki_tanio@yahoo.co.jp>
2019-08-09 17:29:43 +0900

    データ取得修正

--- app/controllers/user_products_sold_controller.rb
+++ app/controllers/user_products_sold_controller.rb
@@ -1,9 +1,8 @@
 class UserProductsSoldController < ApplicationController
   before_action :ransack_set
   before_action :authenticate_user!
-  before_action :current_user_set, only:[:index]
 
   def index
-    @products = @user.products.where(transaction_status: "sold").page(params[:page]).per(30)
+    @products = current_user.products.sold.page(params[:page])
   end
 end
