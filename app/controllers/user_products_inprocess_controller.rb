0e7ec2dc5d0f39e5fa689a526d7e7b065c17205f
Yuki Tanio <yuki_tanio@yahoo.co.jp>
2019-08-09 15:01:38 +0900

    データ取得修正, kaminari設定追加

--- app/controllers/user_products_inprocess_controller.rb
+++ app/controllers/user_products_inprocess_controller.rb
@@ -1,9 +1,8 @@
 class UserProductsInprocessController < ApplicationController
   before_action :ransack_set
   before_action :authenticate_user!
-  before_action :current_user_set, only:[:index]
 
   def index
-    @products = @user.products.where(transaction_status: "inprocess").page(params[:page]).per(30)
+    @products = current_user.products.inprocess.page(params[:page])
   end
 end
