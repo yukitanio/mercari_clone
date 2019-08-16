bd6c0c5f865f4a25919ee778acfa5d8c16e4c304
Yuki Tanio <yuki_tanio@yahoo.co.jp>
2019-08-16 14:02:00 +0900

    likesコントローラー修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -11,8 +11,10 @@ class LikesController < ApplicationController
   def create
     like = current_user.likes.build(product: @product)
     if like.save
+      flash[:notice] = "いいね！しました"
       redirect_back(fallback_location: root_path)
     else
+      flash[:notice] = "いいね！できませんでした。再度実行してください。"
       redirect_back(fallback_location: root_path)
     end
   end
@@ -20,8 +22,10 @@ class LikesController < ApplicationController
   def destroy
     like = Like.find_by(user_id: params[:id], product_id: @product)
     if like.destroy
+      flash[:notice] = "いいね！取り消しました"
       redirect_back(fallback_location: root_path)
     else
+      flash[:notice] = "いいね！取り消しできませんでした。再度実行してください。"
       redirect_back(fallback_location: root_path)
     end
   end
